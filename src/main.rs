2e3ff23d0253ffc0cfd98616f4a2eb81eb5352df
timstr <straubinger.tim@gmail.com>
2024-03-23 15:07:17 -0700

    Switch to using kuchikiki html parser

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,140 @@
-use std::{collections::HashMap, fs, io, path};
-
 use clap::Parser;
+use kuchikiki::{traits::*, NodeRef};
+use std::{cell::RefCell, collections::HashMap, fs, io, path};
 
 // Look for and replace single instances of a named tag with
 // the given replacement
-fn substitute_tag(
-    node: html_parser::Node,
-    tag_name: &str,
-    replacement: &html_parser::Node,
-) -> html_parser::Node {
-    let html_parser::Node::Element(mut elem) = node else {
-        return node;
+fn substitute_tag(node: kuchikiki::NodeRef, tag_name: &str, replacement: &kuchikiki::NodeRef) {
+    let kuchikiki::NodeData::Element(elem) = node.data() else {
+        return;
     };
-    if elem.name == tag_name {
-        return replacement.clone();
+    if &*elem.name.local == tag_name {
+        node.insert_before(replacement.deep_clone());
+        node.detach();
+        return;
     }
-    for child in &mut elem.children {
-        // TODO: avoid this clone
-        *child = substitute_tag(child.clone(), tag_name, replacement);
+    for child in node.children() {
+        substitute_tag(child, tag_name, replacement);
     }
-    html_parser::Node::Element(elem)
 }
 
-// Process a list of nodes, recursively substituting and applying rules,
-// and return the resulting list of nodes, which may have shrunk or grown.
-fn substitute_invocation(
-    nodes: Vec<html_parser::Node>,
-    invocation: &html_parser::Element,
-) -> Vec<html_parser::Node> {
-    nodes
-        .into_iter()
-        .map(|node| -> Vec<html_parser::Node> {
-            // comments and text get passed through unmodified
-            let html_parser::Node::Element(mut elem) = node else {
-                return vec![node];
-            };
+// Process a node, recursively substituting and applying rules, and inserting
+// any resulting nodes in its place
+fn substitute_invocation(node: kuchikiki::NodeRef, invocation: kuchikiki::NodeRef) {
+    // comments and text get passed through unmodified
+    let elem_name: String = if let Some(elem) = node.as_element() {
+        elem.name.local.to_string()
+    } else {
+        return;
+    };
 
-            // substitute innermost elements
-            let children = std::mem::replace(&mut elem.children, vec![]);
-            elem.children = substitute_invocation(children, invocation);
+    // substitute innermost elements
+    for child in node.children() {
+        substitute_invocation(child, invocation.clone());
+    }
 
-            // substitute foreach tags
-            if elem.name == "foreachchild" {
-                assert!(elem.attributes.len() == 1);
-                let (loop_var, val) = elem.attributes.iter().next().unwrap().clone();
-                assert!(val.is_none());
-                assert!(elem.children.len() == 1);
-                return invocation
-                    .children
-                    .iter()
-                    .map(|inv_child| {
-                        let n = substitute_tag(elem.children[0].clone(), &loop_var, &inv_child);
-                        n
-                    })
-                    .collect();
+    // substitute foreach tags
+    if elem_name == "foreachchild" {
+        let attributes = node.as_element().unwrap().attributes.borrow();
+        assert!(attributes.map.len() == 1);
+        let (loop_var, val) = attributes.map.iter().next().unwrap().clone();
+
+        assert!(val.value.is_empty());
+
+        debug_assert!(node.children().filter(|c| c.as_element().is_some()).count() == 1);
+
+        let node_child = node
+            .children()
+            .filter(|c| c.as_element().is_some())
+            .next()
+            .unwrap();
+
+        for inv_child in invocation.children() {
+            if inv_child.as_element().is_none() {
+                continue;
             }
+            let ch = node_child.deep_clone();
+            node.insert_before(ch.clone());
+            substitute_tag(ch, &loop_var.local, &inv_child);
+        }
+        node.detach();
+        return;
+    }
 
-            // Look for tags of the form <self.xyz>
-            let Some(attr_name) = elem.name.strip_prefix("self.") else {
-                // Pass the node through unmodified otherwise
-                return vec![html_parser::Node::Element(elem)];
-            };
-            if attr_name == "inner" {
-                // replace tags <self.inner> with the node's children
-                invocation.children.clone()
-            } else if let Some(attr_val) = invocation.attributes.get(attr_name) {
-                // replace tags <self.xyz> with attribute value xyz if defined
-                if let Some(attr_val) = attr_val {
-                    vec![html_parser::Node::Text(attr_val.to_string())]
-                } else {
-                    vec![]
-                }
-            } else {
-                println!("Warning: undefined attribute <self.{}>", attr_name);
-                vec![]
-            }
-        })
-        .flatten()
-        .collect()
+    // Look for tags of the form <self.xyz>
+    let Some(attr_name) = elem_name.strip_prefix("self.") else {
+        // Pass the node through unmodified otherwise
+        return;
+    };
+
+    if attr_name == "inner" {
+        // replace tags <self.inner> with the node's children
+        for ch in invocation.children() {
+            node.insert_before(ch.deep_clone());
+        }
+        node.detach();
+        return;
+    } else if let Some(attr_val) = invocation
+        .as_element()
+        .unwrap()
+        .attributes
+        .borrow()
+        .get(attr_name)
+    {
+        // replace tags <self.xyz> with attribute value xyz if defined
+        if !attr_val.is_empty() {
+            node.insert_before(NodeRef::new(kuchikiki::NodeData::Text(RefCell::new(
+                attr_val.to_string(),
+            ))));
+        }
+        node.detach();
+    } else {
+        println!(
+            "Warning: undefined attribute self.{} on node <{}>",
+            attr_name, elem_name
+        );
+        println!("Valid attributes are:");
+        for attr in invocation
+            .as_element()
+            .unwrap()
+            .attributes
+            .borrow()
+            .map
+            .iter()
+        {
+            println!("    {:?}", attr);
+        }
+    }
 }
 
 struct ElementDefinition {
     tag_name: String,
-    node: html_parser::Node,
+    node: kuchikiki::NodeRef,
 }
 
 impl ElementDefinition {
     fn from_file(path: &std::path::Path) -> Result<ElementDefinition, io::Error> {
         let name = path.file_stem().unwrap().to_str().unwrap().to_string();
         let source_text = fs::read_to_string(path)?;
-        let dom = html_parser::Dom::parse(&source_text).unwrap();
-        assert!(dom.children.len() == 1);
-        let node = dom.children.into_iter().next().unwrap();
+
+        let document = kuchikiki::parse_fragment(
+            kuchikiki::QualName {
+                prefix: None,
+                ns: kuchikiki::Namespace::from("html"),
+                local: kuchikiki::LocalName::from(""),
+            },
+            Default::default(),
+        )
+        .one(source_text);
+
+        // outer document
+        let document = document.children().next().unwrap();
+        // outer html element
+        let document = document.children().next().unwrap();
 
         Ok(ElementDefinition {
             tag_name: name,
-            node,
+            node: document,
         })
     }
 
@@ -103,12 +142,11 @@ impl ElementDefinition {
         &self.tag_name
     }
 
-    fn instantiate(&self, invocation: &html_parser::Element) -> html_parser::Node {
-        let mut node = self.node.clone();
+    fn instantiate(&self, invocation: kuchikiki::NodeRef) -> kuchikiki::NodeRef {
+        let node = self.node.deep_clone();
 
-        if let html_parser::Node::Element(ref mut node) = &mut node {
-            let children = std::mem::replace(&mut node.children, vec![]);
-            node.children = substitute_invocation(children, invocation);
+        for child in node.children() {
+            substitute_invocation(child, invocation.clone());
         }
 
         node
@@ -141,16 +179,18 @@ impl ElementLibrary {
     }
 }
 
-fn substitute(node: &mut html_parser::Node, library: &ElementLibrary) -> bool {
-    let html_parser::Node::Element(element) = node else {
+fn substitute(node: kuchikiki::NodeRef, library: &ElementLibrary) -> bool {
+    let Some(element) = node.as_element() else {
         return false;
     };
+    let element_name = element.name.local.to_string();
 
     let mut any_substitutions = false;
 
+    // TODO: does this need to be done both before and after?
     loop {
         let mut did_anything = false;
-        for child in &mut element.children {
+        for child in node.children() {
             if substitute(child, library) {
                 did_anything = true;
             }
@@ -160,36 +200,24 @@ fn substitute(node: &mut html_parser::Node, library: &ElementLibrary) -> bool {
         }
     }
 
-    let element_name = element.name.clone();
-
     if let Some(element_defn) = library.elements().get(&element_name) {
-        // TODO: do the substitution
-        *node = element_defn.instantiate(&element);
-
-        assert!(
-            if let html_parser::Node::Element(e) = node {
-                e.name != element_name
-            } else {
-                true
-            },
-            "Node was not substituted"
-        );
+        let instatiation = element_defn.instantiate(node.clone());
+        node.insert_before(instatiation);
+        node.detach();
 
         any_substitutions = true;
     }
 
-    if let html_parser::Node::Element(element) = node {
-        loop {
-            let mut did_anything = false;
-            for child in &mut element.children {
-                if substitute(child, library) {
-                    did_anything = true;
-                    any_substitutions = true;
-                }
+    // TODO: see above
+    loop {
+        let mut did_anything = false;
+        for child in node.children() {
+            if substitute(child, library) {
+                did_anything = true;
             }
-            if !did_anything {
-                break;
-            }
+        }
+        if !did_anything {
+            break;
         }
     }
 
@@ -205,20 +233,22 @@ fn generate_file(
         panic!("Source path must be a file: {}", source_path.display());
     }
 
-    if dst_path.exists() {
-        panic!("Output file already exists: {}", dst_path.display());
-    }
+    // if dst_path.exists() {
+    //     panic!("Output file already exists: {}", dst_path.display());
+    // }
 
     let source_text = fs::read_to_string(source_path)?;
-    let mut dom = html_parser::Dom::parse(&source_text).unwrap();
+    let document = kuchikiki::parse_html().one(source_text);
 
-    for node in &mut dom.children {
+    for node in document.children() {
         substitute(node, library);
     }
 
-    let generated_html = dom.to_html();
+    let mut generated_html = Vec::<u8>::new();
+    document.serialize(&mut generated_html)?;
+    let generated_html = String::from_utf8(generated_html).expect("Generated html is not UTF-8");
 
-    println!("{}", generated_html);
+    // println!("{}", generated_html);
 
     fs::write(dst_path, generated_html)?;
 
@@ -234,11 +264,13 @@ fn generate_folder(
         panic!("Source path must be a directory: {}", source_path.display());
     }
 
-    if dst_path.exists() {
-        panic!("Output directory already exists: {}", dst_path.display());
-    }
+    // if dst_path.exists() {
+    //     panic!("Output directory already exists: {}", dst_path.display());
+    // }
 
-    fs::create_dir(dst_path)?;
+    if !dst_path.exists() {
+        fs::create_dir(dst_path)?;
+    }
 
     for entry in fs::read_dir(source_path)? {
         let entry = entry?;
